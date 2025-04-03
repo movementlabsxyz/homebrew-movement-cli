e0e48d40aa9b747b0a301857c2d3a5ba40ab7495
Andy Golay <andygolay@gmail.com>
2025-04-03 10:41:01 -0400

    Update movement.rb

--- Formula/movement.rb
+++ Formula/movement.rb
@@ -1,7 +1,7 @@
 class Movement < Formula
   desc "Movement CLI for interacting with the Movement network"
   homepage "https://github.com/movementlabsxyz/aptos-core"
-  url "https://github.com/movementlabsxyz/aptos-core/releases/download/movement-full-node-v0.0.1-alpha/movement-cli-macos-arm64.tar.gz"
+  url "https://github.com/movementlabsxyz/aptos-core/archive/refs/tags/movement-full-node-v0.0.1-alpha.tar.gz"
   sha256 "baad3ea0f0651057ec0629445f3fe969b0ecf04399a1cdc9a9a0f5c4101afc06"
   version "0.0.1-alpha"
 
